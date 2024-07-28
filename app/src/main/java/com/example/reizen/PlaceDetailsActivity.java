35df98af94fe3248355abf19ac4d8cd6d4d33b6d
d0tb0t <sdipon.sutradhar@gmail.com>
2024-07-28 21:47:46 +0600

    Updated

--- app/src/main/java/com/example/reizen/PlaceDetailsActivity.java
+++ app/src/main/java/com/example/reizen/PlaceDetailsActivity.java
@@ -4,20 +4,24 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.RequestOptions;
 import com.example.reizen.models.PlaceModel;
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
@@ -28,10 +32,14 @@ import java.util.Map;
 
 public class PlaceDetailsActivity extends AppCompatActivity {
 
+
+    DrawerLayout drawerLayout;
+    ImageView navBtn;
+    NavigationView navView;
     ImageView dePlaceImgIV;
     TextView dePlaceNameTV, dePlaceLocTV;
 
-    Button goHotelList,add_to_wishlist, showInMap;
+    Button goHotelList,add_to_wishlist, showInMap,showRestaurants;
 
     FirebaseFirestore db;
     FirebaseUser user;
@@ -41,14 +49,14 @@ public class PlaceDetailsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_place_details);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
 
         user = FirebaseAuth.getInstance().getCurrentUser();
         db = FirebaseFirestore.getInstance();
+
+        drawerLayout = findViewById(R.id.drawerLayout);
+        navView = findViewById(R.id.navView);
+        navBtn = findViewById(R.id.navBtn);
+        navView.bringToFront();
 
         dePlaceNameTV = findViewById(R.id.dePlaceNameTV);
         dePlaceLocTV = findViewById(R.id.dePlaceLocTV);
@@ -56,11 +64,52 @@ public class PlaceDetailsActivity extends AppCompatActivity {
         goHotelList = findViewById(R.id.goHotelList);
         showInMap = findViewById(R.id.showInMap);
         add_to_wishlist = findViewById(R.id.add_to_wishlist);
+        showRestaurants = findViewById(R.id.showRestaurants);
 
         String placeName = getIntent().getStringExtra("placeName");
         String placeLocation = getIntent().getStringExtra("placeLocation");
         String placeImageUrl = getIntent().getStringExtra("placeImgUrl");
 
+        navBtn.setOnClickListener(v-> {
+
+            drawerLayout.open();
+
+        });
+
+        navView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                int id = item.getItemId();
+
+                if (id == R.id.HomeMenu){
+                    Intent intent = new Intent(getApplicationContext(), PlaceListActivity.class);
+                    startActivity(intent);
+                }
+                else if (id == R.id.ProfileMenu){
+                    Intent intent = new Intent(getApplicationContext(), ProfileActivity.class);
+                    startActivity(intent);
+                }
+                else if (id == R.id.WishlistMenu){
+                    Intent intent = new Intent(getApplicationContext(), WishlistActivity.class);
+                    startActivity(intent);
+                }else if (id == R.id.HelpMenu){
+                    Intent intent = new Intent(getApplicationContext(), HelpActivity.class);
+                    startActivity(intent);
+                }
+                else if (id == R.id.AboutMenu){
+                    Intent intent = new Intent(getApplicationContext(), AboutActivity.class);
+                    startActivity(intent);
+                }
+                else if (id == R.id.LogoutMenu){
+                    FirebaseAuth.getInstance().signOut();
+                    startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                    finishAffinity();
+                }
+
+                return false;
+            }
+        });
 
         Glide.with(this)
                 .load(placeImageUrl)
@@ -75,13 +124,25 @@ public class PlaceDetailsActivity extends AppCompatActivity {
 
         goHotelList.setOnClickListener(v-> {
 
-            startActivity(new Intent(getApplicationContext(), HotelListActivity.class));
+            String mapQuery = "http://maps.google.com/maps/search/?api=1&query=hotels+near+" + Uri.encode(placeName.replaceFirst("^\\d+\\.\\s*", ""));
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(mapQuery));
+            startActivity(intent);
+
+
+        });
+
+        showRestaurants.setOnClickListener(v-> {
+
+            String mapQuery = "http://maps.google.com/maps/search/?api=1&query=restaurants+near+" + Uri.encode(placeName.replaceFirst("^\\d+\\.\\s*", ""));
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(mapQuery));
+            startActivity(intent);
+
 
         });
 
         showInMap.setOnClickListener(v-> {
 
-            String map = "http://maps.google.co.in/maps?q=" + placeName;
+            String map = "http://maps.google.co.in/maps?q=" + placeName.replaceFirst("^\\d+\\.\\s*", "");
             Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(map));
             startActivity(intent);
 
@@ -97,7 +158,6 @@ public class PlaceDetailsActivity extends AppCompatActivity {
                     .collection("wishlist")
                     .document(placeName)
                     .set(placeModel);
-
 
             Toast.makeText(this, "Place added to wishlist.", Toast.LENGTH_SHORT).show();
 
